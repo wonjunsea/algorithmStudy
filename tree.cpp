725f995eaf00e4cb95353b34585635a000085c30
wonjunsea <jun040604@naver.com>
2025-07-07 11:03:20 +0900

    다시 고친 이진트리

--- tree.cpp
+++ tree.cpp
@@ -23,14 +23,26 @@ struct bin_tree{
         }
             insert_impl(root,value);
             return;
-        
+
     }
-    void inorder(){
-        inorder_imptl(root);
+    void inorder(){ //중위순회
+        inorder_impl(root);
+    }
+
+    node* delete_value(int value){//노드를 삭제하는 함수
+        root = delete_impl(root,value);
+    }
+
+    node* succesor(node* start){//선행자(자기보다 큰값 중 가장 작은 값)
+        auto current = start->right;
+        while(current&&current->left){//자기자신과 자기자신의 오른쪽 값이 둘 다 NULL값이 되지않게 search
+            current = current->left;
+        }
+        return current;
     }
     private:
         node* find_impl(node* node,const int value){
-            
+
             if(node->num==value){//3.같다면 그때 node의 값을 반환
                 return node;
             }
@@ -45,7 +57,7 @@ struct bin_tree{
 
         }
         void insert_impl(node* current,const int value){
-           
+
             if(value < current->num){//1.작은경우 왼쪽으로 이동
                 if(!current->left){
                     current->left = new node{value,NULL,NULL};
@@ -63,15 +75,39 @@ struct bin_tree{
                 return;
             }
         }
-        void inorder_imptl(node* current){
+        void inorder_impl(node* current){
             if(!current){
                 return;
             }
-            inorder_imptl(current->left);
+            inorder_impl(current->left);
             cout << current->num<< ",";
-            inorder_imptl(current->right);
+            inorder_impl(current->right);
         }
 
+        node* delete_impl(node* pre,const int value){//삭제함수: 삭제할 노드를 찾고,그노드의 값을 현재 노드의 붙여야 함
+
+            if(pre->num < value){//1.오른쪽에 내가 삭제할 값이 있는 경우
+                pre->right = delete_impl(pre->right,value);
+            }else if( value<pre->num){//2.왼쪽에 내가 삭제할 값이 있는 경우
+                pre -> left = delete_impl(pre->left,value);
+            }else{//3.삭제 케이스 삭제할 노드를 만났을때,그 노드에서의 처리
+                if(!pre->left){//1.왼쪽 자식이 없는경우나 자식이 없는경우->삭제하고 오른쪽 값을 반환함
+                    auto tmp = pre->right;
+                    delete pre;
+                    return tmp;
+                }
+                if(!pre->right){//2.오른쪽 자식이 없는경우->삭제하고 왼쪽 값을 반환함
+                    auto tmp = pre->left;
+                    delete pre;
+                    return tmp;
+                }
+                //3.둘 다 자식이 있는경우? 삭제 하고 어떻게?{선행자의 값을 삭제할 노드에 넣고,서브 트리에서 선행자를 삭제}
+                auto tmp = succesor(pre);
+                pre->num = tmp->num;
+                pre->right = delete_impl(pre->right,tmp->num);
+            }
+            return pre;
+        }
 };
 
 int main(void){
@@ -85,6 +121,8 @@ int main(void){
     tree.insert(28);
     tree.insert(4);
     tree.insert(2);
+    tree.inorder();
+    tree.delete_value(4);
     tree.inorder();
     exit(0);
 }
